e32ffc3819894d011ff51ace07ea3aa726c5c0c3
Ghadeer Mobasher <53903145+mobashgr@users.noreply.github.com>
2024-10-30 15:26:33 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 #
 # These NER datasets are directly 
-#reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (https://drive.google.com/file/d/1cGqvAm9IZ_86C4Mj7Zf-w9CFilYVDl8j/view) and
-#BioRED dataset is publically avaliable in https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/. The datasets are used for fine-tuing from scratch
+#reterived from BioBERT (https://github.com/dmis-lab/biobert) via this link (http://nlp.dmis.korea.edu/projects/biobert-2020-checkpoints/datasets.tar.gz) and
+#BioRED dataset is publically available at https://ftp.ncbi.nlm.nih.gov/pub/lu/BioRED/
 
 set -e
 
 # Configure download location
 DOWNLOAD_PATH="$BIOBERT_DATA"
 if [ "$BIOBERT_DATA" == "" ]; then
-    echo "BIOBERT_DATA not set; downloading to default path ('data')."
+    echo "BIOBERT_DATA not set; downloading to the default path ('data')."
     DOWNLOAD_PATH="./data"
 fi
 DOWNLOAD_PATH_TAR="$DOWNLOAD_PATH.zip"
 
 # Download datasets
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0" -O "$DOWNLOAD_PATH_TAR" && rm -rf /tmp/cookies.txt
+wget 'https://drive.usercontent.google.com/download?id=1nHH3UYpQImQhBTei5HiTcAAFBvsfaBw0&export=download&authuser=1&confirm=t' -O "$DOWNLOAD_PATH_TAR" && rm -rf /tmp/cookies.txt
 jar xvf "$DOWNLOAD_PATH_TAR"
 rm "$DOWNLOAD_PATH_TAR"
 echo "Bio dataset download done!"
-
-mkdir BC7T2-evaluation_v3
-cd  BC7T2-evaluation_v3
-
-
-# BC7T2-evaluation_v3 folder is directly 
-#reterived from ncbi.nlm.nih.gov via this link (https://ftp.ncbi.nlm.nih.gov/pub/lu/BC7-NLM-Chem-track/BC7T2-evaluation_v3.zip)
-wget https://ftp.ncbi.nlm.nih.gov/pub/lu/BC7-NLM-Chem-track/BC7T2-evaluation_v3.zip
-
-jar xvf  BC7T2-evaluation_v3.zip
-rm -r BC7T2-evaluation_v3.zip
-
-echo "BC7T2-evaluation_v3 download done!"
